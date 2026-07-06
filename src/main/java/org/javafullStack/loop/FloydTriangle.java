fbe4000f1488d4fd1d002941dd5270529e773019
alok1Shree <shree.alok705000@gmail.com>
2026-07-06 11:46:14 +0530

    Java Loop pattern

---
+++ src/main/java/org/javafullStack/loop/FloydTriangle.java
@@ -0,0 +1,21 @@
+package org.javafullStack.loop;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+//        1
+//        2 3
+//        4 5 6
+//        7 8 9 10
+//        11 12 13 14 15
+
+        int rows = 5;
+        int number = 1;
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
